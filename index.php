c87690af2e598a2b884359a77b7357f0ff6c0c7b
Oscar Mangut <mangutduranoscar@gmail.com>
2023-11-24 20:13:46 +0100

    Integrando bootstrap

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php include("cabecera.php"); ?>
 
-    Hola develoteca, soy la página de inicio
+    <div class="p-5 bg-light">
+        <div class="container">
+            <h1 class="display-3">Bienvenid@</h1>
+            <p class="lead">Esto es un portfolio personal</p>
+            <hr class="my-2">
+            <p>Más inormación </p>
+            
+        </div>
+    </div>
 
 <?php include("pie.php"); ?>
