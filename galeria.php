2b7afcf0d9feff8ec3f6f08f49390e9482a58a01
Oscar Mangut <mangutduranoscar@gmail.com>
2023-11-27 19:59:51 +0100

    Sección de galeria

--- galeria.php
+++ galeria.php
@@ -1,5 +1,62 @@
 <?php include("cabecera.php"); ?>
-    Hola develoteca, soy tu portfolio
+    <br/>
 
-</body>
-</html>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                
+            <div class="card">
+        <div class="card-header">
+            Datos del proyecto:
+        </div>
+        <div class="card-body">
+        <form action="galeria.php" method="post">
+
+            Nombre del proyecto: <input class="form-control" type="text" name="nombre" id="">
+            <br/>
+            Imagen del proyecto: <input class="form-control" type="file" name="archivo" id="">
+            <br/>
+
+
+            <input class="btn btn-success" type="submit" value="Enviar proyecto">
+        </form>
+        </div>
+        
+    </div>
+            </div>
+            <div class="col-md-6">
+                
+            <table class="table">
+    
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th >Nombre</th>
+            <th >Imagen</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td >3</td>
+            <td>Aplicación web </td>
+            <td>imagen.jpg</td>
+        </tr>
+    </tbody>
+</table>
+            
+            </div>
+        </div>
+    </div>
+
+
+
+
+ 
+
+
+ 
+    
+
+    
+
+<?php include("pie.php"); ?>
