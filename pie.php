64d550eecce561792b5a463f3620f8ef67a98b91
Oscar Mangut <mangutduranoscar@gmail.com>
2023-12-15 17:17:18 +0100

    Creado contenido en la página pie

---
+++ pie.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi Página</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha384-fz5IRNa9tt5qtGZ0zOjlRg/jF+7MKRZbjoO+u9MWkPLaqNQ0BGLTs2MokfZECwSf" crossorigin="anonymous">
+    <!-- Enlace al archivo CSS opcional para personalizar el diseño -->
+    <!-- <link rel="stylesheet" href="styles.css"> -->
+</head>
+<body>
+
+    <header>
+       
+    </header>
+
+    <nav>
+        <ul>
+            <li><a href="https://github.com/oscarmangutduran" target="_blank"><i class="fab fa-github"></i> GitHub</a></li>
+            <li><a href="https://www.linkedin.com/in/oscarmangutduran/" target="_blank"><i class="fab fa-linkedin"></i> LinkedIn</a></li>
+        </ul>
+    </nav>
+
+    <footer>
+        <p>&copy; 2023 Mi Página</p>
+    </footer>
+
+</body>
+</html>
